f3edd534536fc82ddbb546b49998a27d161b5bc5
魏延青 <seedsam@163.com>
2019-06-19 11:05:04 +0800

    工程重新部署,项目名称修改,端口修改

--- eureka_client_ribbon_8083/src/main/java/com/example/demo/HelloWorld.java
+++ eureka_client_ribbon_8083/src/main/java/com/example/demo/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,8 +11,9 @@ public class HelloWorld {
 	@Autowired
 	RestTemplate restTemplate;
 
+	@GetMapping("/")
 	public String home() {
-		return restTemplate.getForObject("http://eureka_client_1_8081/", String.class);
+		return restTemplate.getForObject("http://eureka-client/", String.class);
 	}
 
 }
