b209e813cc9ee14e1fc11e4e13c9bb68315458e3
魏延青 <seedsam@163.com>
2019-06-19 10:39:03 +0800

    eureka-client工程重命名

--- eureka_client_8081/src/main/java/com/example/demo/HelloWorld.java
+++ eureka_client_8081/src/main/java/com/example/demo/HelloWorld.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
+	@GetMapping("/")
 	public String home() {
-		return "eureka_client_1_8081";
+		return "eureka_client_8081";
 	}
 
 }
