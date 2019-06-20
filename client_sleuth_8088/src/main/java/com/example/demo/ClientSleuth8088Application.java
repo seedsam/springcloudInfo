e83561b54174f958d6e0f796a866bf8eed185d55
魏延青 <seedsam@163.com>
2019-06-20 14:23:33 +0800

    新建sleuth-client工程

---
+++ client_sleuth_8088/src/main/java/com/example/demo/ClientSleuth8088Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSleuth8088Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSleuth8088Application.class, args);
+	}
+
+}
