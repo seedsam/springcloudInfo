fa64866602e068d2db2bc497b7aee5a4abbb4aa0
魏延青 <seedsam@163.com>
2019-06-20 17:50:45 +0800

    新建admin-server、client工程

---
+++ admin_client_8091/src/main/java/com/example/demo/AdminClient8091Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClient8091Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClient8091Application.class, args);
+	}
+
+}
