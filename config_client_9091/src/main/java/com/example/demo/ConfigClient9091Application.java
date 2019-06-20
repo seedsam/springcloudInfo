e05ed0623373b6866cebf659305d083b050a35b4
魏延青 <seedsam@163.com>
2019-06-20 08:57:29 +0800

    配置中心修改

--- config_client_9091/src/main/java/com/example/demo/ConfigClient9091Application.java
+++ config_client_9091/src/main/java/com/example/demo/ConfigClient9091Application.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServer9090Application {
+public class ConfigClient9091Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServer9090Application.class, args);
+		SpringApplication.run(ConfigClient9091Application.class, args);
 	}
 
 }
--- eureka_client_config_8087/src/main/java/com/example/demo/EurekaClient1Application.java
+++ eureka_client_config_8087/src/main/java/com/example/demo/EurekaClient1Application.java
@@ -2,14 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServer9090Application {
+@EnableEurekaClient
+//@EnableDiscoveryClient
+public class EurekaClient1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServer9090Application.class, args);
+		SpringApplication.run(EurekaClient1Application.class, args);
 	}
 
 }
